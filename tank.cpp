0f7b86aa7f7e793c4dc5dd2f3040eec6b40cbf96
NiboHoge <56707346+NiboHoge@users.noreply.github.com>
2022-11-17 10:17:08 +0800

    Update tank.cpp

--- tank.cpp
+++ tank.cpp
@@ -1,27 +1,51 @@
 #pragma once
 #include "object.cpp"
+const double pi=acos(-1);
+class object{
+	public :
+		object() {
+			x=y=direct=0,state = -1;
+		}
+		object (double dx,double dy,double dir){
+			x=dx,y=dy;		
+			direct=dir;
+			state = 0;
+		}
+		object (const object &other){
+			x=other.x,y=other.y,direct=other.direct,state=other.state;
+		}
+		bool collapase(object other) {}
+		//virtual void ticking();
+		~object(){}
+		int get_x() { return x; }
+		int get_y() { return y; }
+		int get_direction() { return direct; }
+		int get_state() { return state; }
+	protected :
+		double x,y;
+		int state;
+		double direct;
+};
 class tank :public object {
 public:
-	int speed;
-	tank(int dx, int dy, int dir) :object(dx, dy, dir), speed(1) {};
-	void rotate(tank& thing);
+	double speed;
+	double wspeed;//弧度制 
+	tank(double dx, double dy, double dir) :object(dx, dy, dir), speed(1),wspeed(0.001) {};
+	void rotate(int x);
 	void shoot();
 	void ticking();
 };
-void tank::ticking()
+void tank::ticking()//按右键，dir>0,左键，dir<0 
 {
-	switch (direct)
-	{
-	case 1: x += speed; break;
-	case 2: x -= speed; break;
-	case 3: y += speed; break;
-	case 4: y -= speed; break;
-	}
+	double dx,dy;
+	x+=speed*(sin(direct));
+	y+=speed*(cos(direct));
 }
-/*void tank::rotate(tank& thing)
-{
-	thing.object::direct = read();
+void tank::rotate(int x){//提供左转<0 右转>0 
+	if(x>0) direct+=wspeed;
+	if(x<0) direct-=wspeed; 
 }
+/*
 void tank::shoot()
 {
 	create_bullet();
