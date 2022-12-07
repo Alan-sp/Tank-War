af0595a6b41a8e628fcf4e12a1a634c1e50ff430
NiboHoge <56707346+NiboHoge@users.noreply.github.com>
2022-12-07 19:13:36 +0800

    Update tank.cpp

--- tank.cpp
+++ tank.cpp
@@ -6,12 +6,28 @@ class tank :public object {
 public:
 	double speed;
 	double wspeed;//弧度制 
-	tank(double dx, double dy, double dir) :object(dx, dy, dir), speed(1), wspeed(0.001) { state = 2; };
+	buff my_buff;
+	int name;
+	tank(double dx, double dy, double dir,int m) :object(dx, dy, dir), speed(1), wspeed(0.001) { state = 2; my_buff.buff_state=-1;name=m};
 	void rotate(int x);
 	void shoot();
 	void ticking();
+	void get_buff(buff x);
+	void create_bullet();
 	void collapse(object * other, collapse_result result);
 };
+void tank::create_bullet(){
+	bullet *nw=new bullet(x,y,dir,name);
+	if(my_buff.buff_state!=0)	my_buff.buff_cnt++;
+	nw.my_buff=my_buff;
+	Q_buff(nw);
+	if(my_buff.buff_cnt==20)	my_buff.buff_cnt=my_buff.buff_state=0;
+	return ;
+}
+void tank::get_buff(buff x){
+	my_buff=x;
+	return ;
+}
 void tank::collapse(object * other, collapse_result result) {
 	return;
 }
@@ -25,8 +41,7 @@ void tank::rotate(int x){//提供左转<0 右转>0
 	if(x>0) direct+=wspeed;
 	if(x<0) direct-=wspeed; 
 }
-/*
 void tank::shoot()
 {
 	create_bullet();
-}*/
+}
